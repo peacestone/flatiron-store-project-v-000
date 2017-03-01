fe2071be0f131add13e3a43e36f51db1b7cf8a82
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-01 10:02:09 -0500

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,19 +1,13 @@
 class LineItemsController < ApplicationController
 
   def create
-    #binding.pry
-
     if current_cart
       current_cart.add_item(params[:item_id])
-      current_cart.save
     else
       current_user.create_current_cart.add_item(params[:item_id])
       current_user.save
-      current_cart.save
     end
-
-    #binding.pry
-
+    current_cart.save
     redirect_to cart_path(current_cart)
   end
 
